4bdbdb963119c93a3e8840646c27f75dd8090b81
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-10-29 19:50:02 +0200

    testing for errors

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -42,3 +42,38 @@ it("should yield a correct sum if an array of numeric string values is provided"
     );
     expect(result).toBe(expectedResult);
 });
+
+it("should yield 0 if an empty array is provided", () => {
+    const numbers = [];
+
+    const result = add(numbers);
+
+    expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", () => {
+    // try and catch
+
+    // try {
+    //     const result = add();
+    // } catch (error) {
+    //     expect(error).toBeDefined();
+    // }
+
+    const resultFn = () => {
+        add();
+    };
+    expect(resultFn).toThrow(/is not iterable/);
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+    const num1 = 1;
+    const num2 = 2;
+
+    const resultFn = () => {
+        add(num1, num2);
+    };
+
+    //add regular expression to check for specific error to contain passed value
+    expect(resultFn).toThrow(/is not iterable/);
+});
