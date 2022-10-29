ccd29fa87c27e29bab6a563b503ec189e8b5258a
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-10-29 16:15:35 +0200

    Test rewritten according to AAA (Arrange, Act, Assert)

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -7,6 +7,18 @@ import { add } from "./math";
 // it or test expects 2 arguments:  a string describing the test, and a function with the test
 
 it("should summarize all number values in an array", () => {
-    const result = add([1, 2, 3]);
-    expect(result).toBe(6);
+    // Arrange, Act, Assert
+
+    //Arrange
+    const numbers = [1, 2, 3];
+
+    //Act
+    const result = add(numbers);
+
+    //Assert (here we are settinging the expected value dynamically)
+    const expectedResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue,
+        0
+    );
+    expect(result).toBe(expectedResult);
 });
