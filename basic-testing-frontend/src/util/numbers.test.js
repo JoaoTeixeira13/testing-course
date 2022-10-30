2dc7ac561c499d0a0daf7d56fb2c2946a9d8c5c9
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-10-30 17:15:07 +0100

    Number validation tests.

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -15,17 +15,36 @@ it("should transform and return a passed string number value as a number", () =>
     expect(result).toBeTypeOf("number");
 });
 
-it("should return an invalid string input as NaN", () => {
+it("should transform and return a passed string number value as a number", () => {
     // Arrange, Act, Assert
 
     //Arrange
-    const input = "invalid";
+    const input = "1";
+    // const input = "invalid"; //test will fail
 
     //Act
     const result = transformToNumber(input);
 
     //Assert (here we are setting the expected value dynamically)
+    expect(result)
+        .toBe(+input)
+        .not.toBeNaN();
+});
+
+it("should return an invalid string input as NaN", () => {
+    // Arrange, Act, Assert
+
+    //Arrange
+    const input = "invalid";
+    const input2 = {};
+
+    //Act
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    //Assert (here we are setting the expected value dynamically)
     expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 });
 
 it("should return an empty string as zero", () => {
