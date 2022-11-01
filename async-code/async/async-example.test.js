e05b22033e2c6ca91983272272efe4ed13d6b343
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-01 16:27:15 +0100

    testing asynchronous code

--- async-code/async/async-example.test.js
+++ async-code/async/async-example.test.js
@@ -1,9 +1,8 @@
 import { it, expect, describe } from "vitest";
 
-import { generateToken } from "./async-example";
+import { generateToken, generateTokenPromise } from "./async-example";
 
 describe("generateToken()", () => {
-    
     //testing asynchronous code using callbacks
 
     it("should generate a  token value", (done) => {
@@ -17,5 +16,23 @@ describe("generateToken()", () => {
                 done(err);
             }
         });
+    });
+
+    //testing asynchronous code with Promises && async / await
+
+    it("should generate a  token value", () => {
+        const testUserEmail = "test@test.com";
+
+        return expect(
+            generateTokenPromise(testUserEmail)
+        ).resolves.toBeDefined();
+    });
+
+    it("should generate a  token value", async () => {
+        const testUserEmail = "test@test.com";
+
+        const token = await generateTokenPromise(testUserEmail);
+
+        expect(token).toBeTypeOf("string");
     });
 });
