c75b75ed3c0ff424162797cf8e682cbdc4eebd3b
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-25 20:42:09 +0100

    concurrency

--- hooks/hooks.test.js
+++ hooks/hooks.test.js
@@ -1,4 +1,12 @@
-import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from "vitest";
+import {
+    it,
+    expect,
+    beforeAll,
+    beforeEach,
+    afterEach,
+    afterAll,
+    describe,
+} from "vitest";
 
 import { User } from "./hooks";
 
@@ -24,7 +32,11 @@ afterAll(() => {
     console.log("afterAll()");
 });
 
-it("should update the email", () => {
+//if added here, concurrent executes all tests inside the suite concurrently
+//global state manipulations may clash with each other with concurrency
+// describe.concurrent()
+
+it.concurrent("should update the email", () => {
     const newTestEmail = "test2@test.com";
 
     user.updateEmail(newTestEmail);
@@ -32,22 +44,25 @@ it("should update the email", () => {
     expect(user.email).toBe(newTestEmail);
 });
 
-it("should have an email property", () => {
+it.concurrent("should have an email property", () => {
     expect(user).toHaveProperty("email");
 });
 
-it("should store the provided email value", () => {
+it.concurrent("should store the provided email value", () => {
     expect(user.email).toBe(testEmail);
 });
 
-it("should clear the email", () => {
+it.concurrent("should clear the email", () => {
     user.clearEmail();
 
     expect(user.email).toBe("");
 });
 
-it("should still have an email property after clearing the email", () => {
-    user.clearEmail();
+it.concurrent(
+    "should still have an email property after clearing the email",
+    () => {
+        user.clearEmail();
 
-    expect(user).toHaveProperty("email");
-});
+        expect(user).toHaveProperty("email");
+    }
+);
