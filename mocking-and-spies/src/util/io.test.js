74dc963dad0112fd41691ab967b54556d4c335b7
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-26 12:30:33 +0100

    mocks introduction

--- mocking-and-spies/src/util/io.test.js
+++ mocking-and-spies/src/util/io.test.js
@@ -1,9 +1,19 @@
-import { it, vitest, expect } from "vitest";
+import { it, vitest, expect, vi } from "vitest";
+import { promises as fs } from "fs";
 
 import writeData from "./io";
+
+//jest does not hoist mocks, vitest does (import mock at top when using jest)
+
+vi.mock("fs");
 
 it("should execute the writeFile method", () => {
     const testData = "Test";
     const testFilename = "test.txt";
-    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+    // return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+
+    writeData(testData, testFilename);
+
+    expect(fs.writeFile).toBeCalled();
 });
