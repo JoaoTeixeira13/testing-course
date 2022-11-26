f502d07fd0e94273e961e38dc985548e7f7b4b47
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-26 23:43:09 +0100

    test specific mocking logic

--- mocking-advanced/util/http.test.js
+++ mocking-advanced/util/http.test.js
@@ -1,12 +1,17 @@
 import { it, describe, expect, vi } from "vitest";
+import { HttpError } from "./errors";
 import { sendDataRequest } from "./http";
 
 //mocking globally availabe functions (ex: fetch)
 
 const testResponseData = { testKey: "testData" };
 
-const testFetch = vi.fn((url, optios) => {
+const testFetch = vi.fn((url, options) => {
     return new Promise((resolve, reject) => {
+        if (typeof options.body !== "string") {
+            return reject("Not a string.");
+        }
+
         const testResponse = {
             ok: true,
             json() {
@@ -26,6 +31,40 @@ describe("sendDataRequest()", () => {
         const testData = { key: "test" };
         return expect(sendDataRequest(testData)).resolves.toEqual(
             testResponseData
+        );
+    });
+
+    it("should convert the provided data to JSON before sending the request", async () => {
+        const testData = { key: "test" };
+
+        let errorMessage;
+
+        try {
+            await sendDataRequest(testData);
+        } catch (error) {
+            errorMessage = error;
+        }
+
+        expect(errorMessage).not.toBe("Not a string.");
+    });
+
+    it("should throw an HttpError in case of non-ok responses", () => {
+        testFetch.mockImplementationOnce((url, options) => {
+            return new Promise((resolve, reject) => {
+                const testResponse = {
+                    ok: false,
+                    json() {
+                        return new Promise((resolve, reject) => {
+                            resolve(testResponseData);
+                        });
+                    },
+                };
+                resolve(testResponse);
+            });
+        });
+        const testData = { key: "test" };
+        return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(
+            HttpError
         );
     });
 });
