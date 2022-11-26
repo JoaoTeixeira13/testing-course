e9551f6d825e202816ac100ece40316ff142d862
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-26 12:09:11 +0100

    spy functions

--- mocking-and-spies/src/data.js
+++ mocking-and-spies/src/data.js
@@ -1,17 +1,17 @@
-import writeData from './util/io.js';
+import writeData from "./util/io.js";
 
 export function generateReportData(logFn) {
-  const data = 'Some dummy data for this demo app';
-  if (logFn) {
-    logFn(data);
-  }
+    const data = "Some dummy data for this demo app";
+    if (logFn) {
+        logFn(data);
+    }
 
-  return data;
+    return data;
 }
 
 export async function storeData(data) {
-  if (!data) {
-    throw new Error('No data received!');
-  }
-  await writeData(data, 'data.txt');
+    if (!data) {
+        throw new Error("No data received!");
+    }
+    await writeData(data, "data.txt");
 }
---
+++ mocking-and-spies/src/util/io.test.js
@@ -0,0 +1,9 @@
+import { it, vitest, expect } from "vitest";
+
+import writeData from "./io";
+
+it("should execute the writeFile method", () => {
+    const testData = "Test";
+    const testFilename = "test.txt";
+    return expect(writeData(testData, testFilename)).resolves.toBeUndefined();
+});
