0d8ce5e00178afe00161e65afadd9dd667a8a3c6
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-11-27 12:54:05 +0100

    ReadMe file

---
+++ README.md
@@ -0,0 +1,13 @@
+# ⚙️ Javascript Unit Testing Course
+
+## 🔬 Maximilian Schwarzmüller's Udemy Class 
+
+
+### Course Content:
+
+* 🧪 Unit Tests
+* 🧪 Integration Tests
+* 🧪 Async Tests
+* 🧪 Hooks
+* 🧪 Mocking and Spies
+* 🧪 Testing and the DOM
--- testing-DOM/util/dom.test.js
+++ testing-DOM/util/dom.test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 
-import { it, describe, expect, vi } from "vitest";
+import { it, expect, vi, beforeEach } from "vitest";
 import { Window } from "happy-dom";
 
 import { showError } from "./dom";
@@ -11,9 +11,36 @@ const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(htmlDocumentContent);
 vi.stubGlobal("document", document);
 
-it("first test", () => {
+beforeEach(() => {
+    document.body.innerHTML = "";
+    document.write(htmlDocumentContent);
+});
+
+it('should add an error paragraph to the id="errors" element', () => {
     showError("test");
+
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).not.toBeNull();
+});
+
+it("should contain an error paragraph initially", () => {
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).toBeNull();
+});
+
+it("should output the provided message in the error paragraph", () => {
+    const testErrorMessage = "Test";
+
+    showError(testErrorMessage);
+
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph.textContent).toBe(testErrorMessage);
 });
